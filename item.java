3241bcffcc54da79074390c5efec8ac4c9b668ba
oliver <oliverplug@hotmail.com>
2023-02-22 20:19:40 +0000

    item constructor

--- item.java
+++ item.java
@@ -5,5 +5,17 @@
     private string buyerName
     private double price;
     private int yearSold;
-    private string itemType
- }
+    private string itemType;
+
+// constructor
+
+    public Item(int lotNum, string buyerName, double price, int yearsold, string itemType){
+        this.lotNum = lotNum;
+        this.buyerName = buyerName;
+        this.price = price;
+        this.yearSold = yearsold;
+        this.itemType = itemType;
+
+    }
+
+
