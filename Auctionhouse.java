4b60b6e2311ddb4566ca6629614ce0790aa32343
oliver <oliverplug@hotmail.com>
2023-02-24 02:49:17 +0000

    auction house item price constructors

--- Auctionhouse.java
+++ Auctionhouse.java
@@ -19,5 +19,38 @@ public class Auctionhouse {
 
     }
 
+    public double getAvgPriceForYear(int year) {
+        double total = 0;
+        int count = 0;
+        for (item Item : soldItems) {
+            if (Item.getYearSold() == year) {
+                total += Item.getPrice();
+                count++;
+            }
+        }
+    }   if (count == 0) {
+        return 0;
+    }   else {
+        return total / count;
+    }
 
-}
+    public List<Item> getItemsWithPriceGreaterThan(double price) {
+        List<Item> expensiveItems = new ArrayList<Item>();
+        for (Item item : soldItems) {
+            if (item.getPrice() > price) {
+                expensiveItems.add(item);
+            }
+        }
+        return expensiveItems;
+    }
+
+    public Item getHighestPricedItem() {
+        Item highestPricedItem = null;
+        for (Item item : soldItems) {
+            if (highestPricedItem == null || item.getPrice() > highestPricedItem.getPrice()) {
+                highestPricedItem = item;
+            }
+        }
+        return highestPricedItem;
+    }
+    }
