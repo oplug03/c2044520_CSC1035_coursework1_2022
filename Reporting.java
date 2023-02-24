b5602e49ae1c3dd23e0a107de5b86b4e7dc3b1f4
oliver <oliverplug@hotmail.com>
2023-02-24 03:44:12 +0000

    highest price and highest average methods

--- Reporting.java
+++ Reporting.java
@@ -7,3 +7,30 @@ public class Reporting {
     public Reporting() {
         auctionHouses = new ArrayList<Auctionhouse>();
     }
+    public void addAuctionHouse(Auctionhouse auctionHouse) {
+        auctionHouses.add(auctionHouse);
+    }
+
+    public Item getHighestPriceItem() {
+        Item highestPriceItem = null;
+        double highestPrice = 0.0;
+        for (Auctionhouse auctionHouse : auctionHouses) {
+            for (Item item : auctionHouse.getSoldItems()) {
+                if (item.getPrice() > highestPrice) {
+                    highestPrice = item.getPrice();
+                    highestPriceItem = item;
+                }
+            }
+        }
+        return highestPriceItem;
+    }
+
+    public Auctionhouse getAuctionHouseWithLargestAveragePrice(int year) {
+        Auctionhouse largestAveragePriceAuctionHouse = null;
+        double largestAveragePrice = 0.0;
+        for (Auctionhouse auctionHouse : auctionHouses) {
+            double averagePrice = auctionHouse.getAveragePriceInYear(year);
+            if (averagePrice > largestAveragePrice) {
+                largestAveragePrice = averagePrice;
+                largestAveragePriceAuctionHouse = auctionHouse;
+
