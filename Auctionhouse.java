6188708eaaa15e5d85d29d89a6e7b3d3c655a2c7
oliver <oliverplug@hotmail.com>
2023-02-24 01:57:52 +0000

    auction house fields and constructor

---
+++ Auctionhouse.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.list;
+
+public class Auctionhouse {
+    string name;
+    list<item> SoldItems = new ArrayList<>();
+
+    public Auctionhouse(string name) {
+        this.name = name;
+        list<item> SoldItems = new ArrayList<item>();
+
+
+
+
+
+
+
+    }
+}
